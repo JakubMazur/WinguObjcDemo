c86edb470de3c1f442d93c4477ef0a3b5d72ea1c
Jakub Mazur <jakub@kettu.pl>
2018-09-05 13:03:30 +0200

    Initial Commit

---
+++ WinguObjCDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WinguObjCDemo
+//
+//  Created by Jakub Mazur on 05/09/2018.
+//  Copyright © 2018 Jakub Mazur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WinguObjCDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WinguObjCDemo
+//
+//  Created by Jakub Mazur on 05/09/2018.
+//  Copyright © 2018 Jakub Mazur. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
